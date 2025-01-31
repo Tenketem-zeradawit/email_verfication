b2b86f9a48a579a9b39e51e38926946038c41f73
Tenketem-zeradawit <titihadas123@gmail.com>
2025-01-31 10:21:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-   "mongodb+srv://tenketem:GdSGR1I7wQbeMC8V@book-stort-mert.tvps9.mongodb.net/?retryWrites=true&w=majority&appName=Book-Stort-MERT"   );
+   "mongodb+srv://tenketem:BjDc3PHwu22tHz3S@book-stort-mert.tvps9.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Book-Stort-MERT");
     console.log("Database Connected");
   } catch (error) {
     console.error(error);
